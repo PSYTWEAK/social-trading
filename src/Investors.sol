dbe45e8b81dbc0ff1990a7d55a964486012c5b34
Nanoissuperior <nanoissuperior@nanoissriorsmbp.home>
2023-02-02 16:55:35 +0000

    import interface

--- src/Investors.sol
+++ src/Investors.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
     error TradeActive();
     error TradeInActive();
     error BalanceNotEnough();
